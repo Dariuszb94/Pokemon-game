174da2fc5fe76ad9fbe5300f8b6d3053ce5800f8
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-03-13 20:02:21 +0100

    style choosing pokemon wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import ChosenPokemon from './components/ChosenPokemon';
 import PokemonsList from './components/PokemonsList';
+
+import styled from 'styled-components';
+
 function App() {
   const [pokemonUrl, passPokemonUrl] = useState('');
 
   return (
-    <div>
+    <ChoosePokemonWrapper>
       <PokemonsList passPokemonUrl={passPokemonUrl} />
       <ChosenPokemon pokemonUrl={pokemonUrl} />
-    </div>
+    </ChoosePokemonWrapper>
   );
 }
 
 export default App;
+
+const ChoosePokemonWrapper = styled.div`
+  display: flex;
+`;
