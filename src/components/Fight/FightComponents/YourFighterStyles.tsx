a49f81489251a7e1e95af6a3f2462dfef8ff39b7
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-05-07 11:37:03 +0200

    move padding

--- src/components/Fight/FightComponents/YourFighterStyles.tsx
+++ src/components/Fight/FightComponents/YourFighterStyles.tsx
@@ -19,7 +19,7 @@ export const MovePool = styled.div`
 export const Move = styled.button`
   box-sizing: border-box;
   background-color: rgba(0, 0, 0, 0.4);
-  padding: 16px;
+  padding: 12px;
   border-radius: 4px;
   display: flex;
   justify-content: center;
