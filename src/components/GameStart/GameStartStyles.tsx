926bafc305f9e1543115709c48530e8694ce80aa
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-05-13 16:46:21 +0200

    button padding vert

--- src/components/GameStart/GameStartStyles.tsx
+++ src/components/GameStart/GameStartStyles.tsx
@@ -21,7 +21,7 @@ export const Header = styled.h1`
 export const Button = styled.button`
   outline: none;
   border: none;
-  padding: 14px 32px;
+  padding: 16px 32px;
   font-size: 28px;
   font-weight: bold;
   cursor: pointer;
