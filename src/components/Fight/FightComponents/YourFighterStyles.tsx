f08a1004a05a61ad2765376b35b5a287f690f0f8
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-05-24 22:11:22 +0200

    move color

--- src/components/Fight/FightComponents/YourFighterStyles.tsx
+++ src/components/Fight/FightComponents/YourFighterStyles.tsx
@@ -18,7 +18,7 @@ export const MovePool = styled.div`
 `;
 export const Move = styled.button`
   box-sizing: border-box;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.35);
   padding: 12px;
   border-radius: 6px;
   display: flex;
